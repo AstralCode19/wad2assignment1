c09377c687a8feca515e3efbd0939a80499ca037
Laisha <ljordan1813@gmail.com>
2025-03-17 18:56:58 +0000

    Created now playing movies page and added error messages.

---
+++ movies/src/pages/nowPlayingMoviesPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { getNowPlaying } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "../components/spinner";
+import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
+
+const NowPlayingMovies = (props) => {
+    const {data, error, isPending, isError} = useQuery({
+        queryKey: ['nowplaying'],
+        queryFn: getNowPlaying,
+    })
+
+    if(isPending) {
+        return <Spinner />
+    }
+
+    if (isError) {
+        return <h1>{error.message}</h1>
+    }
+
+    const movies = data.results;
+}
