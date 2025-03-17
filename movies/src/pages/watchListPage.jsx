ed5eb5d8ef62d6949a8c1ba61eac62f31209b0e2
Laisha <ljordan1813@gmail.com>
2025-03-17 14:21:57 +0000

    getting context and querying in watch list.

--- movies/src/pages/watchListPage.jsx
+++ movies/src/pages/watchListPage.jsx
@@ -1,5 +1,20 @@
 import React, {useContext} from "react";
-import {getMovie} from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
+import {getMovie} from "../api/tmdb-api";
 import { MoviesContext } from "../contexts/moviesContext";
 import Spinner from "../components/cardIcons/";
+import RemoveFromPlaylist from "../components/cardIcons/removeFromPlaylist";
+import { useQueries } from "@tanstack/react-query";
+
+const WatchListPage = () => {
+    const {mustWatch: movieIds} = useContext(MoviesContext);
+
+    const watchListMovieQueries = useQueries({
+        queries: movieIds.map((movieId) => {
+            return {
+                queryKey: ['movie', {id: movieId}],
+                queryFn: getMovie,
+            }
+        })
+    });
+}
